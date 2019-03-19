51ebfdd448f489e1e617345806a527f07cdaa147
Lena <obushnieva@Deniss-iMac.local>
2019-03-19 15:48:32 +0200

    add quote search

--- main.go
+++ main.go
@@ -3,17 +3,25 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/Syfaro/telegram-bot-api"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
 	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 )
 
 
-const MSGPATTERN = "работ"
+const (
+	MSGPATTERN = "работ"
+	QSOURCE    = "https://ru.wikiquote.org/wiki/%D0%A2%D1%80%D1%83%D0%B4"
+)
 var (
 	bot      *tgbotapi.BotAPI
 	botToken string
@@ -29,8 +37,6 @@ func initTelegram() {
 		return
 	}
 
-	// this perhaps should be conditional on GetWebhookInfo()
-	// only set webhook if it is not set properly
 	url := baseURL + bot.Token
 	_, err = bot.SetWebhook(tgbotapi.NewWebhook(url))
 	if err != nil {
@@ -55,27 +61,28 @@ func webhookHandler(c *gin.Context) {
 		return
 	}
 
-	match, err := regexp.MatchString(MSGPATTERN, update.Message.Text)
+	match, err := regexp.MatchString(MSGPATTERN, strings.ToLower(update.Message.Text))
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	// to monitor changes run: heroku logs --tail
-	log.Printf("From: %+v Text: %+v\n", update.Message.From, update.Message.Text)
 	if match {
+		quote, err := getQuote()
+		if err != nil {
+			quote = "╭∩╮( ͡° ͜ʖ ͡°)╭∩╮"
+		}
+
 		response := gin.H{
 			"chat_id": update.Message.Chat.ID,
 			"reply_to_message_id": update.Message.MessageID,
-			"text": "╭∩╮( ͡° ͜ʖ ͡°)╭∩╮",
+			"text": quote,
 		}
 		bts, err := json.Marshal(response)
 		if err != nil {
 			log.Println(err)
 			return
 		}
-
-	log.Println("https://api.telegram.org/bot"+ botToken + "/sendMessage")
 
 		resp, err := http.DefaultClient.Post("https://api.telegram.org/bot"+ botToken + "/sendMessage", "application/json", bytes.NewReader(bts))
 		if resp.Status != "200" {
@@ -84,6 +91,42 @@ func webhookHandler(c *gin.Context) {
 		}
 	}
 }
+
+func getQuote() (string, error) {
+	// Request the HTML page.
+	res, err := http.Get(QSOURCE)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return "", errors.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", err
+	}
+
+	sel := doc.Find(`<table cellspacing="0" style="background-color:transparent;text-align:left;">`)
+	n := rand.Intn(sel.Length())
+	// Find the review items
+
+	var bandRes string
+	sel.Each(func(i int, s *goquery.Selection) {
+		if s.Index() == n {
+			// For each item found, get the band and title
+			band := s.Find("tbody").Find("tr").Find("td").Has("div").Text()
+			//title := s.Find("i").Text()
+			fmt.Printf("Review %d: %s - %s\n", i, band)
+			bandRes = band
+		}
+	})
+
+	return bandRes, nil
+}
+
 
 func main() {
 	port := os.Getenv("PORT")
